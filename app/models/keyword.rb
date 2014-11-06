057b5c4200eff9bc8c3c53b46776253568f02099
Jonathan Pichot <jonathan@pichot.us>
2014-11-06 11:23:35 -0500

    Improve Keyword string regex for security

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,5 +2,5 @@ class Keyword < ActiveRecord::Base
   # keyword can be a maximum of 20 characters, cannot have spaces, can begin
   # with a hashtag or an alphanumeric character
   validates :string, length: {minimum: 3, maximum: 20}, presence: true,
-    format: { with: /^[[[:alnum:]]|#][\w|-]+$/i, on: :create }
+    format: { with: /\A[[[:alnum:]]|#][\w|-]+\z/i, on: :create }
 end
