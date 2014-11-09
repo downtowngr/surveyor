62e4d94879e9bf7cd2e6b82fde95c42a446ef36d
David Groulx <david@sandbendersoftware.com>
2014-11-08 20:48:57 -0500

    SingleVoteStrategy enforces uniqueness

--- app/services/single_vote_strategy.rb
+++ app/services/single_vote_strategy.rb
@@ -1,10 +1,20 @@
 class SingleVoteStrategy
   def self.process_text(poll, text)
-    choice = poll.poll_choices.find_by(name: text.keyword)
+    poll_choice = poll.poll_choices.find_by(name: text.keyword)
     citizen = Citizen.find_or_create_by(phone_number: text.from)
 
     # Check if this citizen has already voted
+    current_vote = poll_choice.find_by(citizen_id: citizen.id)
 
-    choice.votes.create(citizen_id: citizen.id)
+    # Keep remove and add in a transaction to not lose the users vote
+    ActiveRecord::Base.transaction do
+      if current_vote
+        # Remove the current vote
+        current_vote.destroy
+      end
+
+      # Regardless, create the new vote
+      choice.votes.create(citizen_id: citizen.id)
+    end
   end
 end
