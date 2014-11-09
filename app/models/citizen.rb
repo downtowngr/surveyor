c49f6279ff3fdbd4d00768c412582da678942865
Jonathan Pichot <jonathan@pichot.us>
2014-11-09 11:20:50 -0500

    SingleVoteStrategy is now enforced

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,8 +1,12 @@
 class Citizen < ActiveRecord::Base
   has_many :votes
   has_many :poll_choices, through: :votes
-  
+
   normalize_attribute :phone_number, with: :phone
-  
+
   validates :phone_number, presence: true, uniqueness: true, length: { in: 10..15 }
+
+  def current_vote(poll)
+    poll_choices.find_by(poll: poll)
+  end
 end
---
+++ spec/factories/vote.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :vote do
+    citizen
+    poll_choice
+  end
+end
