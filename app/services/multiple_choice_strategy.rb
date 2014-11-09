766c35a23d26d6efcd43f7973a12104907bba20c
David Groulx <david@sandbendersoftware.com>
2014-11-08 23:27:50 -0500

    User edit path

--- app/services/multiple_choice_strategy.rb
+++ app/services/multiple_choice_strategy.rb
@@ -1,5 +1,7 @@
 class MultipleChoiceStrategy
   def self.process_text(poll, text)
-    
+    poll_choice = poll.poll_choices.find_by(name: text.keyword)
+    citizen = Citizen.find_or_create_by(phone_number: text.from)
+    poll_choice.votes.create(citizen_id: citizen.id)
   end
 end
