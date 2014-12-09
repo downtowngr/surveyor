72073afd96b5acece8314b8634e988e57d36d50a
Jonathan Pichot <jonathan@pichot.us>
2014-12-09 13:58:11 -0500

    Question and tests.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,27 @@
 class Question < ActiveRecord::Base
+  CITIZEN_ATTRIBUTES = {email: "email"}
+
   belongs_to :blast
-  has_one :number_listener, as: :listening, dependent: :destroy
 
+  has_many :number_listeners, as: :listening, dependent: :destroy
+  # Responsible for collecting answer, storing it
 
+  def respond_to(text, citizen)
+    citizen.send(citizen_attribute + "=", text.body)
+    citizen.nationbuilder_tags = nationbuilder_tags
+
+    citizen.save!
+    citizen.sync_to_nationbuilder!
+
+    destroy_listener(citizen)
+    TwilioOutbound.perform_async(citizen.e164_phone, autoresponse)
+  end
+
+  def create_listener(citizen)
+    number_listeners.create(number: citizen.phone_number)
+  end
+
+  def destroy_listener(citizen)
+    number_listeners.find_by_number(citizen.phone_number).destroy!
+  end
 end
