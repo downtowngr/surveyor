e3a7b886567372c6a0abb32213fe86fab05283e9
Jonathan Pichot <jonathan@pichot.us>
2014-12-09 13:59:05 -0500

    Citizen and spec with cassettes for syncing to nationbuilder

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -9,6 +9,8 @@ class Citizen < ActiveRecord::Base
 
   validates :phone_number, presence: true, uniqueness: true, length: { is: 11 }
 
+  attr_accessor :nationbuilder_tags
+
   def localized_phone
     PhoneNumber.new(phone_number).localized
   end
@@ -21,14 +23,23 @@ class Citizen < ActiveRecord::Base
     poll_choices.where(poll: poll)
   end
 
-  def mobile_opt_out!
-    get_nationbuilder_id if nationbuilder_id.nil?
-    $nb.call(:people, :update, id: nationbuilder_id, person: {mobile_opt_in: false})
+  def sync_to_nationbuilder!
+    response = $nb.call(:people, :push,
+                         person: {
+                           id: nationbuilder_id,
+                           mobile: localized_phone,
+                           email: email,
+                           mobile_opt_in: mobile_opt_in
+                         })
+
+    update(nationbuilder_id: response["person"]["id"]) if nationbuilder_id.nil?
+    add_tags!(nationbuilder_tags) if nationbuilder_tags.present?
   end
 
-  def add_tag(tag)
-    get_nationbuilder_id if nationbuilder_id.nil?
-    $nb.call(:people, :tag_person, id: nationbuilder_id, tagging: {tag: tag})
+  def add_tags!(tags)
+    tags.each do |tag|
+      $nb.call(:people, :tag_person, id: nationbuilder_id, tagging: {"tag" => tag})
+    end
   end
 
   def self.update_or_create_from_nationbuilder(person)
@@ -48,24 +59,5 @@ class Citizen < ActiveRecord::Base
 
     citizen.save!
     citizen
-  end
-
-  private
-
-  def get_nationbuilder_id
-    response = $nb.call(:people, :match, mobile: localized_phone)
-
-    if response["code"] == "no_matches" || response["code"] == "multiple_matches"
-      person = $nb.call(:people, :create, person: {mobile: mobile})["person"]
-    else
-      person = response["person"]
-    end
-
-    update_attributes(
-      nationbuilder_id: person["id"],
-      email: person["email"],
-      full_name: person["full_name"],
-      mobile_opt_in: person["mobile_opt_in"]
-    )
   end
 end
