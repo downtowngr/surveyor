d51d6f55f7a9da931c675a7d7990cadc5d8e7cb2
Jonathan Pichot <jonathan@pichot.us>
2014-12-16 13:16:55 -0500

    VCR match on path

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = "cassettes"
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.default_cassette_options = { match_requests_on: [:method, :path] }
 end
