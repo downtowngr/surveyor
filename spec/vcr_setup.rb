8d6a8494ca07599f7e5c534452dd87cc1454d350
Jonathan Pichot <jonathan@pichot.us>
2014-12-16 13:29:49 -0500

    Ignore codeclimate.com by VCR

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -5,4 +5,5 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.default_cassette_options = { match_requests_on: [:method, :path] }
+  c.ignore_hosts "codeclimate.com"
 end
